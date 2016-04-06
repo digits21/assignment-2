d3d7d5e30fccc19765cbf639e8e435c72a75ce59
digits21 <ouedmenga@gmail.com>
2016-04-06 21:57:31 +0800

    Create Prog2.cpp

---
+++ Prog2.cpp
@@ -0,0 +1,25 @@
+	
+	int* intArray=NULL;
+while(1){
+	intArray=new int[N];
+	for(int i=0;i<N;i++)
+	intArray[i]=0;
+	
+	int size=0;
+	
+	
+	
+ while(1)	{
+	 
+	 cin>>t;
+	
+	 if(t=='x')
+	 break;
+/*	 if(t-'0'<0)
+	 check++;*/
+	 
+	 //cout<<(int)t<<endl;
+	 intArray[size++]=t;
+	 
+	
+	} 
